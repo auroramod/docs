579290bd433c4ea0259c1409209f75d0b367f1b9
m <mjkzyalt@gmail.com>
2022-06-25 07:54:18 -0500

    use default deploymentBranch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'h1-mod', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
-  deploymentBranch: 'publish',
+  //deploymentBranch: 'publish',
   trailingSlash: 'false',
 
   presets: [
